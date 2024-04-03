eaf9e6bb9e560722963b0bc279deb5b824e3e350
Anusha <Anusha@192.168.1.9>
2024-04-03 23:20:47 +0530

    drive

--- src/jdbc_project1/drive.java
+++ src/jdbc_project1/drive.java
@@ -1,10 +1,10 @@
 package jdbc_project1;
 
-public class java {
+public class drive {
 
 	public static void main(String[] args) {
-		System.out.println("full stack");
 		// TODO Auto-generated method stub
+System.out.println("going to drive a car");
 	}
 
 }
